35866e4aa85612afcf4d685c30d2303fd828804c
nivaskodavaty <nivaskodavaty8@gmail.com>
2024-05-14 18:26:29 +0530

    Bug fixes

--- app/src/main/java/com/example/mytodo/data/Todo.kt
+++ app/src/main/java/com/example/mytodo/data/Todo.kt
@@ -1,12 +1,10 @@
 package com.example.mytodo.data
 
-import androidx.room.PrimaryKey
 import io.realm.kotlin.types.RealmObject
 import org.mongodb.kbson.ObjectId
 import java.util.UUID
 
 open class Todo : RealmObject {
-    @PrimaryKey
     var id: String=UUID.randomUUID().toString()
     var title: String = ""
     var description: String = ""
